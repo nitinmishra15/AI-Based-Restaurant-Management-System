1e871cdde278774f7aac575e39219a357bd8ac26
nitinmishra15 <nitinmishra1500@gmail.com>
2026-08-13 00:39:21 +0530

    Fix provider import case

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import '@fontsource/poppins';
-import { CartProvider } from './app/providers/cartProvider.jsx'
+import { CartProvider } from './app/providers/CartProvider.jsx'
 import { AuthProvider } from './app/providers/AuthContextApi/AuthProvider.jsx'
 import { MenuProvider } from './app/providers/MenuContextApi/MenuProvider.jsx'
 import { TableProvider } from './app/providers/TableContextApi/TableProvider.jsx'
